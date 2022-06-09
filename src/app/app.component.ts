3f2b2838d19ffb8b1acb0c14af83d485f042efc9
Tomas Svojanovsky <tomas.svojanovsky11@gmail.com>
2022-06-09 16:53:39 +0200

    ngclass start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+type Person = {
+  name: string;
+  balance: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  people: Person[] = [
+    {
+      name: "Josef",
+      balance: 500,
+    },
+    {
+      name: "Ales ",
+      balance: 5000,
+    },
+    {
+      name: "Tomas",
+      balance: -15000,
+    },
+    {
+      name: "Aneta",
+      balance: 2000,
+    },
+    {
+      name: "Petra",
+      balance: -1000,
+    },
+    {
+      name: "Jiri",
+      balance: 1400,
+    },
+  ];
 
+  trackByFn(index: number) {
+    return index;
+  }
 }
