28a3a9ccfa30986f0e1bf08a1f68871ae0ad5965
Tomas Svojanovsky <tomas.svojanovsky11@gmail.com>
2022-06-11 14:49:46 +0200

    Todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  todos: string[] = [];
 
+  newTodo = "";
+
+  onKeyUp(event: KeyboardEvent) {
+    const { value } = event.target as HTMLInputElement;
+    this.newTodo = value;
+  }
+
+  onClick() {
+    this.todos = [...this.todos, this.newTodo];
+    this.newTodo = "";
+  }
 }
