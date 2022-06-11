2bc7235b3d46fe62db0b3ee43a28be1bf8cec1c6
Tomas Svojanovsky <tomas.svojanovsky@seznam.cz>
2022-06-11 08:21:25 +0200

    User lifecycle methods

--- src/app/modules/invoices/pages/invoice-list.component.ts
+++ src/app/modules/invoices/pages/invoice-list.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-invoice-list',
   templateUrl: './invoice-list.component.html',
 })
-export class InvoiceListComponent {
+export class InvoiceListComponent implements OnInit, OnDestroy {
 
+  ngOnInit() {
+    console.log("Hey I am called on the beginning");
+  }
+
+  ngOnDestroy() {
+    console.log("Hey I am called on destroy");
+  }
 }
