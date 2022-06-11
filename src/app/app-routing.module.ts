7d21428ee7866d86ad469da6eb5a269a7cc54eab
Tomas Svojanovsky <tomas.svojanovsky@seznam.cz>
2022-06-11 08:20:15 +0200

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { UserListComponent } from "./modules/users/pages/user-list.component";
+import { InvoiceListComponent } from "./modules/invoices/pages/invoice-list.component";
+import { LayoutContainerComponent } from "./modules/shared/containers/layout-container.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: LayoutContainerComponent,
+    children: [
+      {
+        path: "users",
+        component: UserListComponent,
+      },
+      {
+        path: "users/:id/invoices",
+        component: InvoiceListComponent,
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "/users",
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/invoices/pages/invoice-list.component.ts
+++ src/app/modules/invoices/pages/invoice-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-invoice-list-',
+  selector: 'app-invoice-list',
   templateUrl: './invoice-list.component.html',
 })
 export class InvoiceListComponent {
